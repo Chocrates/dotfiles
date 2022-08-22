a7d65df350b032bd086b7bb52efd91cf8a399731
Chris McIntosh <rougechampion2002@gmail.com>
2022-08-22 13:44:31 -0500

    Update bootstrap.sh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sudo apt update
+sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+
 sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --apply Chocrates
