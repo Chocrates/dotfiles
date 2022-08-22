07782d7ed730cd09731390178b20f497bd682911
Chris McIntosh <rougechampion2002@gmail.com>
2022-08-22 13:56:53 -0500

    Update and rename script/bootstrap.sh to scripts/bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,13 @@
 # Configure zsh as default shell
 sudo chsh -s $(which zsh) vscode
 sudo apt update
+
+# Set timezone
 sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 
+# Install chezmoi and dotfiles from this repository
 sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --apply Chocrates
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
