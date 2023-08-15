136d5c732273fde6975865c954b576dc8135c896
Chris McIntosh <rougechampion2002@gmail.com>
2023-08-15 10:53:33 -0500

    Adding rust and NVIM to my bootstrap
    updating bootstrap such that I have nvim and rust for my codespaces

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
+sudo apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  build-essential \
+  openssl
+
 # Configure zsh as default shell
 sudo chsh -s $(which zsh) vscode
-sudo apt update
+
+# Install neovim
+NVIM_VERSION=0.7.0
+sudo apt-get install -y libfuse2
+curl -L -o $HOME/bin/nvim https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage
+chmod a+x $HOME/bin/nvim
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+rustup install nightly
+rustup component add rustfmt
+rustup component add rustfmt --toolchain nightly
+rustup component add clippy 
+rustup component add clippy --toolchain nightly
 
 # Set timezone
 sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install chezmoi and dotfiles from this repository
 sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --apply Chocrates
-
-# Install oh-my-zsh
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
